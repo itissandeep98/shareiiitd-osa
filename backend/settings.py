acc4e1de84fcc44ccecd3073aae682f606639d06
Sandeep Kumar Singh <itissandeep98@gmail.com>
2021-06-17 15:45:17 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_USER_PASS'),
-        'HOST': os.getenv('DB_HOST),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '5432',
     }
 }
