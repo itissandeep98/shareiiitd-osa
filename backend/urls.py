6084eafb6ecbb60eff5108f246c8462ec30e9913
itissandeep98 <itissandeep98@gmail.com>
2021-06-17 16:44:32 +0530

    admin route added and packages updated

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework_jwt.views import obtain_jwt_token
+from django.contrib import admin
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     path('core/', include('core.urls'))
 ]
